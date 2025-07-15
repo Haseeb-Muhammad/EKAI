f417b0c149590c0070404ab58e2d78613aac69a0
Haseeb Muhammad <huhammad.bscs22seecs@seecs.edu.pk>
2025-07-15 23:49:50 +0200

    feat: calculate similarity between name of dependent and reference attributes of an IND

--- ERD_automation/codes/my_automation/my_automation/ind.py
+++ ERD_automation/codes/my_automation/my_automation/ind.py
@@ -1,15 +1,35 @@
+from fuzzywuzzy import fuzz
+
+
 class IND:
     """
-    A class representing Inclusion Dependencies between attributes.
-    
-    Parameters
-    ----------
-    dependent : Attribute
-        The dependent attribute in the inclusion dependency
-    reference : Attribute
-        The reference attribute in the inclusion dependency
+    Represents an Inclusion Dependency (IND) between two attributes.
+
+    Attributes:
+        dependent: The dependent attribute in the IND relationship.
+        reference: The reference attribute in the IND relationship.
+        name_similarity: A method to calculate the similarity between the names of the dependent and reference attributes.
+
+    Methods:
+        __init__(dependent, reference):
+            Initializes the IND object with the given dependent and reference attributes.
+
+        _calculate_name_similarity():
+            Calculates the similarity score between the attribute names of the dependent and reference attributes using fuzzy string matching.
+            Returns:
+                float: The similarity score as a value between 0 and 1.
     """
-    
     def __init__(self, dependent, reference):
         self.dependent = dependent
         self.reference = reference
+        self.name_similarity = _calculate_name_similarity
+
+        def _calculate_name_similarity(self): 
+            """
+            Calculates the similarity between the attribute names of the dependent and reference objects using the partial ratio method from the `fuzz` library.
+
+            Returns:
+                float: The similarity score as a float between 0 and 1.
+            """
+            similarity = fuzz.partial_ratio(self.dependent.attribute_name, self.reference.attribute_name)
+            return similarity /100
